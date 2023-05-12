ce503ab750b7b0d38ffa86f8bfd386520ad0e1f9
Semjon Gerasimov <simon.gerasimov@gmail.com>
2023-05-12 11:42:36 +0300

    Initial Commit

---
+++ TouristLocations/TouristLocationsApp.swift
@@ -0,0 +1,17 @@
+//
+//  TouristLocationsApp.swift
+//  TouristLocations
+//
+//  Created by mac on 12.05.2023.
+//
+
+import SwiftUI
+
+@main
+struct TouristLocationsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
